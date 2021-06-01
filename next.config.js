4c5f8999e3b2b980b1c6a1482769e96d74f81823
lindsey-s <ly.stevenson@gmail.com>
2021-06-01 16:57:11 -0700

    remove next config file

--- next.config.js
+++
@@ -1,7 +0,0 @@
-// const basePath = process.env.NODE_ENV === 'production' ? '/lindsey-s.github.io' : '';
-const basePath = '';
-
-module.exports = {
-    basePath,
-    assetPrefix: `${basePath}/`,
-};
