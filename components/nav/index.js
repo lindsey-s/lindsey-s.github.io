6431df4ecb2658eabc3acf6816f5eeaac5facb45
lindsey-s <ly.stevenson@gmail.com>
2021-06-05 11:48:29 -0700

    fetch api/pages using swr package in nav component

--- components/nav/index.js
+++ components/nav/index.js
@@ -1,11 +1,35 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { useState } from 'react'
+import useSWR from 'swr'
 
 import styles from './Nav.module.css'
 
+const fetchApi = (url) => fetch(url).then((res) => res.json())
+
 export default function Nav() {
     const [menuOpen, setMenuOpen] = useState(false)
+    const { data, error } = useSWR('/api/pages', fetchApi)
+    
+    function menuContents() {
+        if (error) return <div>Failed to load menu</div>
+        if (!data) return <div>Menu Loading...</div>
+        
+        return (
+            <>
+                Menu
+                <ul className={styles.menuItems}>
+                    {
+                        data.pages.map((route, i) => (
+                            <li key={i}>
+                                <Link href={route.path}>{route.title}</Link>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </>
+        )
+    }
 
     return (
         <>
@@ -17,12 +41,7 @@ export default function Nav() {
             <nav className={styles.nav}>
                 <div className={[menuOpen ? styles.menuOpen : styles.menuClosed, styles.menu].join(' ')}>
                     <div className={styles.menuButton} onClick={() => {setMenuOpen(!menuOpen)}}>
-                        Menu
-                        <ul className={styles.menuItems}>
-                            <li>
-                                <Link href='/'><a>Home</a></Link>
-                            </li>
-                        </ul>
+                        {menuContents()}
                     </div>
                 </div>
             </nav>
