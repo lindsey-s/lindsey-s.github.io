7fe1ee09a700601d76ab59f2980d7e7455f30ac1
lindsey-s <ly.stevenson@gmail.com>
2021-08-03 14:21:09 -0700

    updated resume link

--- components/linkToResume/index.js
+++ components/linkToResume/index.js
@@ -7,7 +7,7 @@ export default function LinkToResume() {
     return (
         <div className={styles.wrapper}>
             <a className={styles.button} target="_blank" rel="noopener noreferrer"
-                href="https://drive.google.com/file/d/1vQwcajqdWvxsDNcXcdnS7-FJ-dc8IWxG/view?usp=sharing">
+                href="https://drive.google.com/file/d/1SZ8cgTNRJckXIseKKk1LWVK2Srv65q7q/view?usp=sharing">
                 <span>Resume</span>
                 <FontAwesomeIcon className={styles.icon} icon={faExternalLinkAlt} />
             </a>
